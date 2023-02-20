52d32a43aa6b055958f70d59b2899f4a219c579e
thanhvt <thanhvt@vissoft.vn>
2023-02-21 01:10:46 +0700

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env bash
+﻿#!/bin/sh
 
 /opt/meadow-payment-service/jetbrains_debugger_agent_20210604.19.0 -port=81 > /opt/meadow-payment-service/debug.log 2>&1 &
 dotnet /opt/meadow-payment-service/Main.dll
